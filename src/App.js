0a8fed5011fac27da8ff48cba2cc41cad0ecacff
Colm <colm@colms-mbp.derrycitycouncil.org>
2018-01-15 15:10:36 +0000

    mailchimp fix

--- src/App.js
+++ src/App.js
@@ -151,25 +151,19 @@ class App extends Component {
               <div className='col-lg-3 col-md-3'>
                 <h3>NEWSLETTER            </h3>
                 <div className='input-group'>
-               
-               
-               
-                {/* ASK CIARAN ABOUT THIS  */}
                   <link href='//cdn-images.mailchimp.com/embedcode/slim-081711.css' rel='stylesheet' type='text/css'/>
                   <div id='mc_embed_signup'>
                     <form action='//derrycityandstrabanedistrict.us9.list-manage.com/subscribe/post?u=af21a5655b0fdf68785275e32&amp;id=4978b17613' method='post' id='mc-embedded-subscribe-form' name='mc-embedded-subscribe-form' className='validate' target='_blank' noValidate=''>
                       <div id='mc_embed_signup_scroll'>
                         <label htmlFor='mce-EMAIL'>Subscribe to our mailing list</label>
-                        <input type='email' value='' name='EMAIL' className='email' id='mce-EMAIL' placeholder='email address' required=''/>
-                        <div > 
-                          {/* <input type='text' name='b_af21a5655b0fdf68785275e32_4978b17613' tabIndex='-1' value=''/> */}
+                        <input type='email' name='EMAIL' className='email' id='mce-EMAIL' placeholder='email address' required=''/>
+                        <div >  
+                          <input type='text' name='b_af21a5655b0fdf68785275e32_4978b17613' tabIndex='-1' value='' hidden/>
                         </div>
                         <p></p>
                         <div className='clear'><input type='submit' value='Subscribe' name='subscribe' id='mc-embedded-subscribe' className='button'/></div>
                       </div>
                     </form>
-                {/* ASK CIARAN ABOUT THIS  */}
-
                   </div>
                 </div>
               </div>
