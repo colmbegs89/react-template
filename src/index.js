5be0bc5eb6ec225c9c4ebe2f526a70d2c52669de
Colm <colm@colms-mbp.derrycitycouncil.org>
2018-01-16 10:56:03 +0000

    Bones of the app

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App'
 import About from './components/About.js'
 import OurBoat from './components/OurBoat.js'
 import NotFound from './components/NotFound.js'
+import Accommodation from './components/Accommodation.js'
+import Contact from './components/Contact.js'
 import registerServiceWorker from './registerServiceWorker'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
@@ -15,6 +17,8 @@ ReactDOM.render(
             <Route  path="/" exact component={App} />
             <Route path="/about" exact component={About} />
             <Route path="/ourboat" exact component={OurBoat} />
+            <Route path="/accommodation" exact component={Accommodation} />
+            <Route path="/contact" exact component={Contact} />
             <Route component={NotFound} />
           </ Switch>
       </div>
