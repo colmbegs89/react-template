c07b17b963b3c8acdf2fa14629e421d5676702c2
Colm <colm@colms-mbp-2.derrycitycouncil.org>
2018-01-30 11:21:37 +0000

    css for hamburger fixed for navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,7 +4,6 @@ export class Navigation extends Component {
   render () {
 
     return (
-      // <div className='container'>
 
       <nav className="navbar navbar-default top-bar navbar-fixed-top" role="navigation">
         <div className="container-fluid">
@@ -56,29 +55,7 @@ export class Navigation extends Component {
           </div>
         </div>
       </nav>
-        // </div>
-
-//         <nav className="navbar navbar-inverse">
-//   <div className="container-fluid">
-//     <div className="navbar-header">
-//       <a class="navbar-brand" href="#">WebSiteName</a>
-//     </div>
-//     <ul className="nav navbar-nav">
-//       <li className="active"><a href="#">Home</a></li>
-//       <li className="dropdown">
-//         <a className="dropdown-toggle" data-toggle="dropdown" href="#">Page 1
-//         <span className="caret"></span></a>
-//         <ul className="dropdown-menu">
-//           <li><a href="#">Page 1-1</a></li>
-//           <li><a href="#">Page 1-2</a></li>
-//           <li><a href="#">Page 1-3</a></li>
-//         </ul>
-//       </li>
-//       <li><a href="#">Page 2</a></li>
-//       <li><a href="#">Page 3</a></li>
-//     </ul>
-//   </div>
-// </nav>
+       
     )
   }
 }
